a122e4e3d4c332531f7d946669827eb400480fbe
Alberto <ercalbwar@gmail.com>
2015-03-15 00:41:14 +0000

    Trying to fix it

---
+++ GoingOn/Frontend/Common/BusinessValidationException.cs
@@ -0,0 +1,21 @@
+﻿// ****************************************************************************
+// <copyright file="BusinessValidationException.cs" company="Universidad de Malaga">
+// Copyright (c) 2015 All Rights Reserved
+// </copyright>
+// <author>Alberto Guerra Gonzalez</author>
+// <summary>
+// TODO: write a summary
+// </summary>
+// ****************************************************************************
+
+namespace GoingOn.Frontend.Common
+{
+    using System;
+
+    public class BusinessValidationException : Exception
+    {
+        public BusinessValidationException(string message) : base(message)
+        {
+        }
+    }
+}
