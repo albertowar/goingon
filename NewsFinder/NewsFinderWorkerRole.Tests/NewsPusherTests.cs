2000642a2644958d0d4f98a0242717b4c8fa003a
Alberto <ercalbwar@gmail.com>
2015-03-28 23:46:45 +0000

    Putting all the code together

---
+++ NewsFinder/NewsFinderWorkerRole.Tests/NewsPusherTests.cs
@@ -0,0 +1,68 @@
+﻿// ****************************************************************************
+// <copyright file="NewsPusherTests.cs" company="Universidad de Malaga">
+// Copyright (c) 2015 All Rights Reserved
+// </copyright>
+// <author>Alberto Guerra Gonzalez</author>
+// <summary>
+// TODO: write a summary
+// </summary>
+// ****************************************************************************
+
+namespace NewsFinderWorkerRole.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using Entities;
+    using GoingOn.Client;
+    using GoingOn.Client.Entities;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NewsPusherTests
+    {
+        private NewsPusher newsPusher;
+
+        private GOClient client;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            string username = ConfigurationManager.AppSettings["NewsFinderWorkerRoleUsername"];
+            string password = ConfigurationManager.AppSettings["NewsFinderWorkerRolePassword"];
+
+            this.client = new GOClient(
+                ConfigurationManager.AppSettings["GoingOnEndpoint"],
+                username,
+                password);
+
+            this.client.CreateUser(new UserClient { Nickname = username, Password = password, City = "Malaga", Name = "Alberto", Email = "alberto@gmail.com" }).Wait();
+
+            this.newsPusher = new NewsPusher();
+        }
+
+        [TestMethod]
+        public void TestPushSingleNews()
+        {
+            var articles = new List<Article>
+            {
+                new Article
+                {
+                    Title = "Title",
+                    Summary = "Summary",
+                    PublishDate = DateTime.Parse("2015-12-05")
+                }
+            };
+
+            this.newsPusher.PushNews(articles).Wait();
+        }
+
+        [TestMethod]
+        public void TestPusAllhNews()
+        {
+            var articles = NewsFinder.FindNews().Result;
+
+            this.newsPusher.PushNews(articles).Wait();
+        }
+    }
+}
---
+++ NewsFinder/NewsFinderWorkerRole/NewsPusher.cs
@@ -0,0 +1,49 @@
+﻿// ****************************************************************************
+// <copyright file="NewsConverter.cs" company="Universidad de Malaga">
+// Copyright (c) 2015 All Rights Reserved
+// </copyright>
+// <author>Alberto Guerra Gonzalez</author>
+// <summary>
+// TODO: write a summary
+// </summary>
+// ****************************************************************************
+
+namespace NewsFinderWorkerRole
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Threading.Tasks;
+    using Entities;
+    using GoingOn.Client;
+    using GoingOn.Client.Entities;
+
+    public class NewsPusher
+    {
+        private GOClient client;
+
+        public NewsPusher()
+        {
+            string username = ConfigurationManager.AppSettings["NewsFinderWorkerRoleUsername"];
+            string password = ConfigurationManager.AppSettings["NewsFinderWorkerRolePassword"];
+
+            this.client = new GOClient(
+                ConfigurationManager.AppSettings["GoingOnEndpoint"], 
+                username, 
+                password);
+
+            this.client.CreateUser(new UserClient { Nickname = username, Password = password, City = "Malaga", Name = "Fedzilla", Email = "alberto@gmail.com" }).Wait();
+        }
+
+        public async Task PushNews(List<Article> articles)
+        {
+            foreach (var article in articles)
+            {
+                // TODO: improve the City
+                var response = await this.client.CreateNews(
+                    "Malaga", 
+                    article.PublishDate.ToString("yyyy-MM-dd"),
+                    NewsConverter.ToNewsClient(article));
+            }
+        }
+    }
+}
