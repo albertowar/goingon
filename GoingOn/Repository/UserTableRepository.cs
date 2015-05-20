ba63482c918f3397c65f41034aeb0d6aaabf4a73
Alberto <ercalbwar@gmail.com>
2015-05-20 22:35:11 +0100

    More stuff

---
+++ GoingOn/Repository/UserTableRepository.cs
@@ -0,0 +1,68 @@
+﻿// ****************************************************************************
+// <copyright file="UserTableRepository.cs" company="Universidad de Malaga">
+// Copyright (c) 2015 All Rights Reserved
+// </copyright>
+// <author>Alberto Guerra Gonzalez</author>
+// <summary>
+// Manages the repository of user data
+// </summary>
+// ****************************************************************************
+
+namespace GoingOn.Repository
+{
+    using System.Threading.Tasks;
+    using GoingOn.Model.EntitiesBll;
+    using GoingOn.Repository.Entities;
+    using GoingOn.XStoreProxy;
+    using GoingOn.XStoreProxy.TableStore;
+    using Microsoft.WindowsAzure.Storage.Table;
+
+    public class UserTableRepository : IUserRepository
+    {
+        private readonly ITableStore _tableStore;
+
+        public UserTableRepository(string connectionString, string tableName)
+        {
+            this._tableStore = new TableStore(connectionString, tableName);
+        }
+
+        public async Task AddUser(UserBll userBll)
+        {
+            await this._tableStore.AddTableEntity(UserEntity.FromUserBll(userBll));
+        }
+
+        public async Task<UserBll> GetUser(string city, string nickname)
+        {
+            return UserEntity.ToUserBll(await this._tableStore.GetTableEntity<UserEntity>(city, nickname));
+        }
+
+        public async Task<bool> ContainsUser(UserBll userBll)
+        {
+            try
+            {
+                await this._tableStore.GetTableEntity<UserEntity>(userBll.City, userBll.Nickname);
+
+                return true;
+            }
+            catch (AzureTableStorageException)
+            {
+                return false;
+            }
+        }
+
+        public async Task UpdateUser(UserBll userBll)
+        {
+            await this._tableStore.UpdateTableEntity(UserEntity.FromUserBll(userBll));
+        }
+
+        public async Task DeleteUser(UserBll userBll)
+        {
+            await this._tableStore.DeleteTableEntity<UserEntity>(userBll.City, userBll.Nickname);
+        }
+
+        public async Task DeleteAllUsers(string city)
+        {
+            await this._tableStore.DeleteAllTableEntitiesInPartition<UserEntity>(city);
+        }
+    }
+}
---
+++ GoingOn/XStoreProxy.Tests/NewsImageBlobStorageTests.cs
@@ -0,0 +1,58 @@
+﻿// ****************************************************************************
+// <copyright file="NewsImageBlobStorageTests.cs" company="Universidad de Malaga">
+// Copyright (c) 2015 All Rights Reserved
+// </copyright>
+// <author>Alberto Guerra Gonzalez</author>
+// <summary>
+// NewsImageBlobRepository tests class
+// </summary>
+// ****************************************************************************
+
+namespace GoingOn.XStoreProxy.Tests
+{
+    using System;
+    using System.Configuration;
+    using System.Drawing;
+    using GoingOn.XStoreProxy.BlobStore;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NewsImageBlobStorageTests
+    {
+        private const string City = "Malaga";
+        private static readonly DateTime date = DateTime.Today;
+        private static readonly Guid NewsId = Guid.NewGuid();
+        private static readonly Image Image = Image.FromFile("goku.png");
+
+        private IImageRepository repository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+            string blobContainerName = ConfigurationManager.AppSettings["ImageBlobContainerName"];
+
+            this.repository = new NewsImageBlobRepository(connectionString, blobContainerName);
+        }
+
+        [TestMethod]
+        public void TestCreateImage()
+        {
+            this.repository.CreateNewsImage(City, date, NewsId, Image).Wait();
+
+            Assert.IsTrue(this.repository.ContainsImage(City, date, NewsId).Result);
+
+            this.repository.DeleteNewsImage(City, date, NewsId).Wait();
+        }
+
+        [TestMethod]
+        public void TestCreateExistingImage()
+        {
+            this.repository.CreateNewsImage(City, date, NewsId, Image).Wait();
+
+            this.repository.CreateNewsImage(City, date, NewsId, Image).Wait();
+        }
+
+        // TODO: implement test for all operations
+    }
+}
